9d93959798c2c621065dc449b39adc0a230de8cf
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-31 10:56:10 +0500

    Ignores

--- selection.js
+++ selection.js
@@ -1,23 +1,23 @@
-const heading1 = document.getElementById('heading1')
-console.log(heading1)
-heading1.remove()
+// const heading1 = document.getElementById('heading1')
+// console.log(heading1)
+// heading1.remove()
 
-const inputEls = document.getElementsByName('username')
-const inputEl = inputEls[0]
-console.log(inputEl)
-
-
-
-const headings = document.getElementsByClassName('heading')
-console.log(headings)
+// const inputEls = document.getElementsByName('username')
+// const inputEl = inputEls[0]
+// console.log(inputEl)
 
 
-const headings = document.getElementsByTagName('h1')
-console.log(headings)
+
+// const headings = document.getElementsByClassName('heading')
+// console.log(headings)
 
 
-const heading1 = document.querySelector('#heading1')
-console.log(heading1)
-//
-const headings = document.querySelectorAll('h1')
-console.log(headings)
+// const headings = document.getElementsByTagName('h1')
+// console.log(headings)
+
+
+// const heading1 = document.querySelector('#heading1')
+// console.log(heading1)
+// //
+// const headings = document.querySelectorAll('h1')
+// console.log(headings)
