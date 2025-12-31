1ef31a029867a332c09afc7e2ae7b9e16ce4261a
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-31 10:41:12 +0500

    new changes

--- selection.js
+++ selection.js
@@ -1,23 +1,23 @@
-// const heading1 = document.getElementById('heading1')
-// console.log(heading1)
-// heading1.remove()
+const heading1 = document.getElementById('heading1')
+console.log(heading1)
+heading1.remove()
 
-// const inputEls = document.getElementsByName('username')
-// const inputEl = inputEls[0]
-// console.log(inputEl)
-// inputEl.remove()
-
-
-// const headings = document.getElementsByClassName('heading')
-// console.log(headings)
-// headings[1].remove()
-
-// const headings = document.getElementsByTagName('h1')
-// console.log(headings)
+const inputEls = document.getElementsByName('username')
+const inputEl = inputEls[0]
+console.log(inputEl)
 
 
-// const heading1 = document.querySelector('#heading1')
-// console.log(heading1)
 
+const headings = document.getElementsByClassName('heading')
+console.log(headings)
+
+
+const headings = document.getElementsByTagName('h1')
+console.log(headings)
+
+
+const heading1 = document.querySelector('#heading1')
+console.log(heading1)
+//
 const headings = document.querySelectorAll('h1')
 console.log(headings)
